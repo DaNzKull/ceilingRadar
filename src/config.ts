85fc7743c83fc4458a22905615edc71fbefb9d74
Danz <danz@gmail.com>
2026-06-29 18:13:31 +0200

    fix

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,9 @@
-export const config = { lat: 47.53, lon: 21.63, radiusNm: 30 };
+const params = new URLSearchParams(window.location.search);
+const lat = Number(params.get("lat")) || 47.53;
+const lon = Number(params.get("lon")) || 21.63;
+const radiusNm = Number(params.get("radius")) || 30;
+
+export const config = { lat, lon, radiusNm };
 
 export const ADSB_URL = import.meta.env.PROD
   ? `https://api.airplanes.live/v2/point/${config.lat}/${config.lon}/${config.radiusNm}`
